28bec2facdbc2769662323a5f21a62125120ae65
Ni Putu Devira <73383559+deviraaam@users.noreply.github.com>
2022-02-19 18:22:01 +0700

    Add files via upload

---
+++ src/textminingkd/TextMiningKD.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package textminingkd;
+import ALI.*;
+import java.util.Map;
+/**
+ *
+ * @author user
+ */
+public class TextMiningKD {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VectorLib vlib = new VectorLib();
+        TextMiningLib tmlib = new TextMiningLib();
+        
+        String dir="berita/";
+String[] docs = {"berita1.txt", "berita2.txt", "berita3.txt"};
+    }
+    
+}
